2d87abb72b6e9fb9032aba07c95a2e2a0ac6dbe0
Maarcus Reniero L <maarcusreniero.l@gmail.com>
2024-01-22 22:42:37 +0530

    feat: add backend functionality for adding new book

--- src/app/actions/create.js
+++ src/app/actions/create.js
@@ -1,10 +1,30 @@
 'use server'
 
-// import { client } from "@/lib/db"
+import { client } from "@/lib/db"
 import { redirect } from 'next/navigation'
 
 export async function createBook(formData) {
-  const {title, rating, author, blurb} = Object.fromEntries(formData)
+  const { title, rating, author, blurb } = Object.fromEntries(formData)
 
-  
+  // Create book id
+  const id = Math.floor(Math.random() * 100_00)
+
+  // add book to the sorted set
+  const unique = await client.zAdd("books", {
+    value: title,
+    score: id,
+  }, {
+    NX: true,
+  })
+
+  if (!unique) {
+    return { error: "Book already exists!" }
+  }
+
+  // save new book
+  await client.hSet(`books:${id}`, {
+    title, rating, author, blurb
+  });
+
+  redirect("/");
 }
