732ccdf9794558eae534a969082d2a248c9c0b11
Maarcus Reniero L <maarcusreniero.l@gmail.com>
2024-01-22 22:51:37 +0530

    feat: add function to fetch book data

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,19 @@
 import Link from 'next/link'
+import { client } from '@/lib/db';
 
 const getBooks = async () => {
-  
+  const result = await client.zRangeWithScores("books", 0, -1);
+
+  const books = await Promise.all(result.map((book) => {
+    return client.hGetAll(`books:${book.score}`)
+  }));
+
+  return books;
 }
 
 export default async function Home() {
+
+  const books = await getBooks();
 
   return (
     <main>
@@ -12,8 +21,15 @@ export default async function Home() {
         <h1 className='font-bold'>Books on Redis!</h1>
         <Link href="/create" className="btn">Add a new book</Link>
       </nav>
-      
-      <p>List of books here.</p>
-    </main>
+
+      {books.map((book) => (
+        <div className="card" key={book.title}>
+          <h2>{book.title}</h2>
+          <p>By {book.author}</p>
+          <p>{book.blurb}</p>
+          <p>Rating: {book.rating}</p>
+        </div>
+      ))}
+    </main >
   )
 }
